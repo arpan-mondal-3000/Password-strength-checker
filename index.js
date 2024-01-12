22525867bba8696475d3d7c833e22d8391ce7602
Arpan Mondal <139039003+arpan-mondal-3000@users.noreply.github.com>
2024-01-12 22:52:16 +0530

    basics done

--- index.js
+++ index.js
@@ -11,28 +11,70 @@ checkbtn.addEventListener("click", () => {
   textbox.value = "";
   let result = checkStrenght(password);
   output.innerText = `Your password, ${password} is ${result}.`;
-  console.log(password);
 });
 
-function containsLetter(password) {
-  let i = 0;
-  let j = 0;
-  for (alpha in letters) {
-    if (alpha in password) {
-      i++;
-    } else if (alpha.toUpperCase() in password) {
-      j++;
+function contents(password) {
+  let content = [0, 0, 0, 0];
+  for (let i = 0; i < password.length; i++) {
+    for (let j = 0; j < letters.length; j++) {
+      if (password[i] === letters[j]) {
+        content[0] = 1;
+      }
     }
   }
-  return [i, j];
+  for (let i = 0; i < password.length; i++) {
+    for (let j = 0; j < letters.length; j++) {
+      if (password[i] === letters[j].toUpperCase()) {
+        content[1] = 1;
+      }
+    }
+  }
+  for (let i = 0; i < password.length; i++) {
+    for (let j = 0; j < nums.length; j++) {
+      if (password[i] === nums[j]) {
+        content[2] = 1;
+      }
+    }
+  }
+  for (let i = 0; i < password.length; i++) {
+    for (let j = 0; j < specialChars.length; j++) {
+      if (password[i] === specialChars[j]) {
+        content[3] = 1;
+      }
+    }
+  }
+  return content[0] + content[1] + content[2] + content[3];
 }
 
 function checkStrenght(password) {
   if (password) {
+    let content = contents(password);
     if (password.length <= 4) {
-      let alpha = containsLetter(password);
-      console.log(alpha[0], alpha[1]);
-      return "weak";
+      if (content === 1) {
+        return "very weak";
+      } else if (content === 2) {
+        return "weak";
+      } else {
+        return "medium";
+      }
+    }
+    if (password.length <= 8) {
+      if (content === 1) {
+        return "weak";
+      } else if (content === 2) {
+        return "medium";
+      } else {
+        return "strong";
+      }
+    }
+    if (password.length > 8) {
+      if (content === 1) {
+        return "medium";
+      } else if (content === 2) {
+        return "strong";
+      } else {
+        return "very strong";
+      }
     }
   }
 }
