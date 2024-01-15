fc9edca9c8b9433b76c0ea3284d18c4ce19ac269
Arpan Mondal <139039003+arpan-mondal-3000@users.noreply.github.com>
2024-01-15 11:36:00 +0530

    completed

--- index.js
+++ index.js
@@ -2,40 +2,42 @@ let checkbtn = document.querySelector("#check");
 let textbox = document.querySelector("#password");
 let output = document.querySelector("#output");
 
-const letters = "abcdefghijklmnopqrstuvwxyz";
+const letters = "abcdefghijklmnopqrstuvwxyz ";
 const nums = "1234567890";
 const specialChars = "!@#$%^&*()_+={}[]/><,.;:?`~'";
 
-checkbtn.addEventListener("click", () => {
+checkbtn.addEventListener("click", async () => {
   let password = textbox.value;
   textbox.value = "";
-  let result = checkStrenght(password);
+  let result = await checkStrenght(password);
   output.innerText = `Your password, ${password} is ${result}.`;
 });
 
-// async function isPasswordCommon(password) {
-//   try {
-//     await fetch("./pass_data.json")
-//       .then((result) => result.json())
-//       .then((data) => {
-//         let res = false;
-//         for (let i = 0; i < data.length; i++) {
-//           let pass = data[i];
-//           console.log(pass);
-//           if (password === pass) {
-//             res = true;
-//             break;
-//           }
-//         }
-//       })
-//       .catch((err) => {
-//         console.log("Error", err);
-//       });
-//     return res;
-//   } catch (error) {
-//     console.error("Error:", error);
-//   }
-// }
+async function isPasswordCommon(password) {
+  try {
+    await fetch("./pass_data.json")
+      .then((result) => result.json())
+      .then((data) => {
+        for (let i = 0; i < data.length; i++) {
+          let pass = data[i];
+          if (password === pass) {
+            res = true;
+            break;
+          } else {
+            res = false;
+          }
+        }
+      })
+      .catch((err) => {
+        console.log("Error", err);
+      });
+    return new Promise((resolve, reject) => {
+      resolve(res);
+    });
+  } catch (error) {
+    console.error("Error:", error);
+  }
+}
 
 function contents(password) {
   let content = [0, 0, 0, 0];
@@ -70,35 +72,44 @@ function contents(password) {
   return content[0] + content[1] + content[2] + content[3];
 }
 
-function checkStrenght(password) {
-  if (password) {
-    let content = contents(password);
-    if (password.length <= 4) {
-      if (content === 1) {
-        return "very weak";
-      } else if (content === 2) {
-        return "weak";
+async function checkStrenght(password) {
+  try {
+    if (password) {
+      let common = await isPasswordCommon(password);
+      if (common === true) {
+        return "very weak, as it is in the most common password list";
       } else {
-        return "medium";
+        let content = contents(password);
+        if (password.length <= 4) {
+          if (content === 1) {
+            return "very weak";
+          } else if (content === 2) {
+            return "weak";
+          } else {
+            return "medium";
+          }
+        }
+        if (password.length <= 8) {
+          if (content === 1) {
+            return "weak";
+          } else if (content === 2) {
+            return "medium";
+          } else {
+            return "strong";
+          }
+        }
+        if (password.length > 8) {
+          if (content === 1) {
+            return "medium";
+          } else if (content === 2) {
+            return "strong";
+          } else {
+            return "very strong";
+          }
+        }
       }
     }
-    if (password.length <= 8) {
-      if (content === 1) {
-        return "weak";
-      } else if (content === 2) {
-        return "medium";
-      } else {
-        return "strong";
-      }
-    }
-    if (password.length > 8) {
-      if (content === 1) {
-        return "medium";
-      } else if (content === 2) {
-        return "strong";
-      } else {
-        return "very strong";
-      }
-    }
+  } catch (error) {
+    console.error("Error:", error);
   }
 }
